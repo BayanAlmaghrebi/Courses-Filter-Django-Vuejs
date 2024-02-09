d1840e4cc9721bdbe2da0a1101e5d66bfa2e4b07
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2024-02-09 15:37:36 +0100

    add Courses List & Detail API & Category List API

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import filters
 
-# Create your views here.
+from .serializers import CourseSerializer , CategorySerializer
+from .models import Course , Category
+
+
+
+class CourseListAPI(generics.ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseDetailAPI(generics.RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+
+class CategoryListAPI(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
