f3a8905f09cead7d15f46d238c93644585e91fa7
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2024-02-09 19:29:49 +0100

    fix category in Course List

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -3,14 +3,17 @@ from .models import Category, Course
 
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category 
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
     class Meta:
         model = Course 
         fields = '__all__'
 
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category 
-        fields = '__all__'
