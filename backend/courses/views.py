4e456fb5ea4c79688da3bf07a29a9c5e1b4d368f
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2024-02-09 20:42:50 +0100

    add courses filter from backend

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,15 +1,17 @@
 from rest_framework import generics
 from rest_framework import filters
-
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CourseSerializer , CategorySerializer
 from .models import Course , Category
-
+from .filters import CourseFilter
 
 
 class CourseListAPI(generics.ListAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category']
+    filterset_class = CourseFilter
 
 class CourseDetailAPI(generics.RetrieveAPIView):
     queryset = Course.objects.all()
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     "corsheaders",
-    # 'django_filter',
+    'django_filters',
 
     'courses',
 ]
